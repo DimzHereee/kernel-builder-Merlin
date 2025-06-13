56116d3934d475005a07c046559ee870db584e83
DimzHere!! <167015498+DimzHereee@users.noreply.github.com>
2025-06-14 03:32:37 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,13 +5,13 @@ DEVICE_CODENAME="merlin"
 DEVICE_MODEL="Redmi Note 9"
 #
 BUILD_USER="DimzMachine"
-BUILD_HOST="PhoneixKernel"
+BUILD_HOST="Phoneix-Next Kernel"
 TIMEZONE="Asia/Jakarta"
 #
-KERNEL_NAME="PhoneixKernel betaa"
+KERNEL_NAME="Phoneix-Next Kernel"
 KERNEL_ARCH="arm64"
-KERNEL_REPO="https://github.com/DimzHereee/android_kernel_xiaomi_mt6768-merlinx"
-KERNEL_BRANCH="aosp-14/lts"
+KERNEL_REPO="https://github.com/DimzHereee/Kernel-merlinx"
+KERNEL_BRANCH="next"
 KERNEL_DEFCONFIG="merlin_defconfig"
 #
 ANYKERNEL_REPO="https://github.com/DimasAkbr29/AnyKernel"
